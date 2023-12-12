0561e8999a7fc5eb2cdf6ca7cfb6cf626ed5393b
gondotcl <clara.gondot@ensta-bretagne.org>
2023-12-12 10:35:05 +0100

    code du debut --clara

---
+++ Lecture_et_extraction/main.cpp
@@ -0,0 +1,11 @@
+//
+// Created by clara on 12/12/23.
+//
+
+#include "utils/au_utils.h"
+
+std::string filename = "data/au_files/au_1.au";
+
+int main(int argc, char** argv) {
+    return 0;
+}
---
+++ Lecture_et_extraction/utils/constants.h
@@ -0,0 +1,14 @@
+//
+// Created by clara on 12/12/23.
+//
+
+#ifndef LOAD_AND_EXTRACT_CONSTANTS_H
+#define LOAD_AND_EXTRACT_CONSTANTS_H
+
+#include <cmath>
+
+const int FFT_SIZE = 1024;
+const float PI = M_PI;
+const int MAGIC_NB = 0x2e736e64;
+
+#endif //LOAD_AND_EXTRACT_CONSTANTS_H
